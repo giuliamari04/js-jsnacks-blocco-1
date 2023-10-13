58e0abd06e56f38cb6ef557afb18cd95ccdbce21
giuliamari04 <giuliamari04@gmail.com>
2023-10-13 13:51:18 +0200

    esercizio cocktail senza css

--- js/script.js
+++ js/script.js
@@ -11,22 +11,31 @@ const ingredienti =[
 ];
 
 let presente = false;
+const lista = document.getElementById('ingredienti');
+const risp = document.getElementById('risposta');
+let listEl='';
 
 ingredienti.push('pesca');
 
 for(let i=0; i < ingredienti.length;i++){
     console.log(ingredienti[i]);
-
+    listEl += ingredienti[i] +' ';
+    
     if(ingredienti[i] ==='cocomero'){
        presente = true;
     }else{
         i++;
     }
 }
+lista.innerHTML = `
+    ${listEl}
+`;
 
 if(presente === true){
     console.log('facciamolo');
+    risp.innerText = 'Trovato! Devo solo preparare i cocktail';
 
 }else{
     console.log('spesa');
+    risp.innerText = 'Oh no, devo uscire a comprare il cocomero!';
 }
